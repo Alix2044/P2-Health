ffc2281cc624e203b29c826aaf88d4ec922816a9
Ali Rahimi <75444456+Alix2044@users.noreply.github.com>
2024-05-16 12:29:02 +0200

    Breakfast

---
+++ helpers/breakfastIngredients.js
@@ -0,0 +1,5 @@
+const breakfastIngredients=["Eggs", "Oats", "Low-fat yogurt", "Bananas", "Milk", "Bread", "Spinach",
+"Seeds", "Berries", "Avocado", "Peanut Butter", "Honey", "Cinnamon", "Almonds", "Skim milk", "Apple", "Orange","Nuts", "Cereal", 
+"Butter", "Flour", "Oil"];
+
+module.exports = breakfastIngredients;
